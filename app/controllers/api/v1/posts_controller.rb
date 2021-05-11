3001838f2d407be7e75459efea8d1e19510bad1d
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-12 06:05:42 +0900

    Send posts API from controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,33 @@
 class Api::V1::PostsController < ApplicationController
   def index
-    post = { 'title' => 'This is post title.', 'body' => 'This is post body.'}
+    posts = [
+      {
+        'id' => 1,
+        'title' => 'This is post one title',
+        'body' => 'This is post one body.'
+      },
+      {
+        'id' => 2,
+        'title' => 'This is post two title',
+        'body' => 'This is post two body.'
+      },
+      {
+        'id' => 3,
+        'title' => 'This is post three title',
+        'body' => 'This is post three body.'
+      },
+      {
+        'id' => 4,
+        'title' => 'This is post four title',
+        'body' => 'This is post four body.'
+      },
+      {
+        'id' => 5,
+        'title' => 'This is post five title',
+        'body' => 'This is post five body.'
+      },
+    ]
 
-    render :json => post
+    render json: posts
   end
 end
