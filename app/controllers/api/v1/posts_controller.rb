ef475b9e7b7a2a7214af76f1135ad433cd2c0712
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-12 06:33:20 +0900

    Add additional posts data

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -26,6 +26,31 @@ class Api::V1::PostsController < ApplicationController
         'title' => 'This is post five title',
         'body' => 'This is post five body.'
       },
+      {
+        'id' => 6,
+        'title' => 'This is post six title',
+        'body' => 'This is post six body.'
+      },
+      {
+        'id' => 7,
+        'title' => 'This is post seven title',
+        'body' => 'This is post seven body.'
+      },
+      {
+        'id' => 8,
+        'title' => 'This is post eight title',
+        'body' => 'This is post eight body.'
+      },
+      {
+        'id' => 9,
+        'title' => 'This is post nine title',
+        'body' => 'This is post nine body.'
+      },
+      {
+        'id' => 10,
+        'title' => 'This is post ten title',
+        'body' => 'This is post ten body.'
+      },
     ]
 
     render json: posts
