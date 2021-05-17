8eeac95921b4c3f30deed56dbcb3d030ad0b3f69
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-18 05:54:01 +0900

    Remove eyecatch update

--- app/controllers/api/v1/eyecatches_controller.rb
+++ app/controllers/api/v1/eyecatches_controller.rb
@@ -1,33 +1,18 @@
 class Api::V1::EyecatchesController < ApplicationController
-  before_action :eyecatch_upload
-
   def create
-    eyecatch = Eyecatch.new(post_id: params[:refId], image: @res['secure_url'])
+    @res = Cloudinary::Uploader.upload(
+      params[:files],
+      :folder => "travellog",
+      :public_id => "eyecatch_" + params[:refId]
+    )
+
+    post = Post.find(params[:refId])
+    eyecatch = post.build_eyecatch(image: @res['secure_url'])
 
     if eyecatch.save
       render json: { status: :ok, data: eyecatch }
     else
       render json: { status: 500, data: eyecatch.errors }
     end
-  end
-
-  def update
-    eyecatch = Eyecatch.find_by(post_id: params[:refId])
-
-    if eyecatch.update(image: @res['secure_url'])
-      render json: { status: :ok, data: eyecatch }
-    else
-      render json: { status: 500, data: eyecatch.errors }
-    end
-  end
-
-  private
-
-  def eyecatch_upload
-    @res = Cloudinary::Uploader.upload(
-      params[:files],
-      :folder => "travellog",
-      :public_id => "eyecatch_" + params[:refId]
-    )
   end
 end
