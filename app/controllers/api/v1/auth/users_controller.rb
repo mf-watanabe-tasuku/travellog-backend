1ab739bfae45625605c251138df99377f709c351
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-22 12:58:13 +0900

    Add user create

--- app/controllers/api/v1/auth/users_controller.rb
+++ app/controllers/api/v1/auth/users_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::Auth::UsersController < ApplicationController
   def create
-    @user = User.nnew(user_params)
-    if @user.save
-      render json: { status: :ok, data: @user }
+    user = User.new(user_params)
+    if user.save
+      render json: { status: :created, data: user }
     else
-      render json: { status: 500, message: 'Something went wrong' }
+      render json: { status: :internal_server_error, message: 'Something went wrong' }
     end
   end
 
