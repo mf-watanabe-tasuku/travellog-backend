7d42512272013f633d408353c3c7b92acdb9db56
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-23 15:18:25 +0900

    Add jwt

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.3'
 
 gem 'bcrypt', '3.1.13'
+gem 'jwt'
 gem 'dotenv-rails'
 gem 'rack-cors'
 gem 'cloudinary'
---
+++ app/controllers/concerns/jwt_authenticator.rb
@@ -0,0 +1,26 @@
+module JwtAuthenticator
+  require 'jwt'
+
+  SECRET_KEY_BASE = Rails.application.secrets.secret_key_base
+
+  def jwt_authenticate
+    if request.headers['Authorization'].blank?
+      render json: { status: "ERROR", message: 'Not authorized' }, status: :unauthorized
+    else
+      encoded_token = request.headers['Authorization'].split('Bearer ').last
+      payload = decode(encoded_token)
+      @user = User.find(payload['user_id'])
+    end
+  end
+
+  def encode(user_id)
+    expires_in = 1.month.from_now.to_i
+    preload = { user_id: user_id, exp: expires_in }
+    JWT.encode(preload, SECRET_KEY_BASE, 'HS256')
+  end
+
+  def decode(encoded_token)
+    decoded_dwt = JWT.decode(encoded_token, SECRET_KEY_BASE, true, algorithm: 'HS256')
+    decoded_dwt.first
+  end
+end
