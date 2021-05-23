9c1c9a81782404e0edabc438f192e2d6d24e0ae1
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-23 15:50:58 +0900

    Add seed data using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,24 @@
-posts = Post.create([
-  {
-    title: 'Post One Title',
-    body: 'This is post one body This is post one body This is post one body This is post one body This is post one body This is post one body This is post one body This is post one body This is post one body This is post one body This is post one body This is post one body This is post one body This is post one body This is post one body This is post one body .'
-  },
-  {
-    title: 'Post Two Title',
-    body: 'This is post two body This is post two body This is post two body This is post two body This is post two body This is post two body This is post two body This is post two body This is post two body This is post two body This is post two body This is post two body This is post two body This is post two body This is post two body This is post two body .'
-  },
-  {
-    title: 'Post Three Title',
-    body: 'This is post three body This is post three body This is post three body This is post three body This is post three body This is post three body This is post three body This is post three body This is post three body This is post three body This is post three body This is post three body This is post three body This is post three body This is post three body This is post three body .'
-  },
-  {
-    title: 'Post Four Title',
-    body: 'This is post four body This is post four body This is post four body This is post four body This is post four body This is post four body This is post four body This is post four body This is post four body This is post four body This is post four body This is post four body This is post four body This is post four body This is post four body This is post four body .'
-  },
-])
+CLOUDINARY_IMAGES = [
+  "https://res.cloudinary.com/dnzkxyfdv/image/upload/v1621204014/travellog/sample_1.jpg",
+  "https://res.cloudinary.com/dnzkxyfdv/image/upload/v1621751832/travellog/sample_2.jpg",
+  "https://res.cloudinary.com/dnzkxyfdv/image/upload/v1621751832/travellog/sample_3.jpg",
+]
+
+20.times do
+  post = Post.create({
+    title: Faker::Company.bs.titlecase,
+    body: Faker::Lorem.paragraph_by_chars(number: 1000, supplemental: false)
+  })
+
+  post.build_eyecatch({
+    image: CLOUDINARY_IMAGES[post.id % 3]
+  })
+end
 
 user = User.create([
   {
-    name: 'tasuku',
-    email: 'tasuku@example.com',
+    name: 'test',
+    email: 'test@example.com',
     password: 'foobar',
     password_confirmation: 'foobar'
   }
